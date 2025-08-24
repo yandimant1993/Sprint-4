fb6fed49f6739c6f0284c2bcefdb89f479d373ed
yandimant1993 <yan1993dimant@gmail.com>
2025-08-25 00:29:34 +0200

    List&Station cmp beggining

--- src/cmps/StationList.jsx
+++ src/cmps/StationList.jsx
@@ -11,18 +11,21 @@ export function StationList({ stations, onRemoveStation, onUpdateStation }) {
         return station.owner?._id === user._id
     }
 
+
     if (!stations) return <p>Loading...</p>
+
     return <section>
         <ul className="station-list">
             {stations.map(station =>
                 <li key={station._id}>
                     <StationPreview station={station}/>
                     {shouldShowActionBtns(station) && <div className="actions">
-                        <button onClick={() => onUpdateStation(station)}>Edit</button>
-                        <button onClick={() => onRemoveStation(station._id)}>x</button>
+                        {/* <button onClick={() => onUpdateStation(station)}>Edit</button>
+                        <button onClick={() => onRemoveStation(station._id)}>x</button> */}
+                        <button onClick={() => playStation()(station._id)}>play</button>
                     </div>}
-                </li>)
-            }
+                </li>
+                )}
         </ul>
     </section>
 }
--- src/cmps/StationPreview.jsx
+++ src/cmps/StationPreview.jsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom'
 
 export function StationPreview({ station }) {
-    return <article className="station-preview">
-        <header>
-            <Link to={`/station/${station._id}`}>{station.name}</Link>
-        </header>
+    return (
+        <article className="station-preview">
+            <p className="station-picture">{}</p>
+            {/* <Link to={`/station/${station._id}`}>{station.name}</Link> */}
 
-        <p>AddedAt: <span>{station.addedat.toLocaleString()} Km/h</span></p>
-        {station.owner && <p>Owner: <span>{station.owner.fullname}</span></p>}
-        
-    </article>
+
+        </article>
+
+    )
 }
