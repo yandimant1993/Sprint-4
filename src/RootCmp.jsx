c85ee35d57be2e1f025c32d237676e2b97c43794
avishaigal <avishai.gal@gmail.com>
2025-08-24 21:55:43 +0300

    temp commit for bug fix

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,20 +1,13 @@
-import React from 'react'
 import { Routes, Route } from 'react-router'
 
-import { HomePage } from './pages/HomePage'
-import { AboutUs, AboutTeam, AboutVision } from './pages/AboutUs'
 import { StationIndex } from './pages/StationIndex.jsx'
-// import { AdminIndex } from './pages/AdminIndex.jsx'
+import { StationDetails } from './pages/StationDetails.jsx'
 
-import { StationDetails } from './pages/StationDetails'
-import { UserDetails } from './pages/UserDetails'
-
-import { AppHeader } from './cmps/AppHeader'
-import { AppFooter } from './cmps/AppFooter'
+import { AppHeader } from './cmps/AppHeader.jsx'
+import { AppFooter } from './cmps/AppFooter.jsx'
 import { SideNav } from './cmps/SideNav.jsx'
 import { UserMsg } from './cmps/UserMsg.jsx'
 import { LoginSignup, Login, Signup } from './pages/LoginSignup.jsx'
-
 
 export function RootCmp() {
     return (
@@ -22,18 +15,10 @@ export function RootCmp() {
             <AppHeader />
             <UserMsg />
             <SideNav />
-
             <main>
                 <Routes>
                     <Route path="/" element={<StationIndex />} />
-                    {/* <Route path="" element={<HomePage />} /> */}
-                    {/* <Route path="/about" element={<AboutUs />}>
-                        <Route path="team" element={<AboutTeam />} />
-                        <Route path="vision" element={<AboutVision />} />
-                    </Route> */}
                     <Route path="station/:stationId" element={<StationDetails />} />
-                    {/* <Route path="user/:id" element={<UserDetails />} /> */}
-                    {/* <Route path="admin" element={<AdminIndex />} /> */}
                     <Route path="auth" element={<LoginSignup />}>
                         <Route path="login" element={<Login />} />
                         <Route path="signup" element={<Signup />} />
