dfc4e091656edd41a72d39a5ace7fe13ee143a86
avishaigal <avishai.gal@gmail.com>
2025-08-25 16:28:15 +0300

    Fix sidenav render with stationdetails

--- src/cmps/SideNav.jsx
+++ src/cmps/SideNav.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react"
+import { useSelector } from 'react-redux'
 import { SideNavHeader } from "./SideNavHeader";
 import { StationList } from "./StationList";
 
-export function SideNav({ stations }) {
+export function SideNav() {
+    const stations = useSelector(storeState => storeState.stationModule.stations)
     const [isExpanded, setIsExpanded] = useState(true)
-    if (!stations) return
+
     return (
         <section className={`side-nav-container ${isExpanded ? 'expanded' : 'collapsed'}`}>
             <SideNavHeader setIsExpanded={setIsExpanded} isExpanded={isExpanded} />
-            <StationList stations={stations} />
+            {stations?.length ? <StationList stations={stations} /> : <p>Loading stations...</p>}
         </section>
     )
 }
