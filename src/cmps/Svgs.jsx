10644208d7f7323947db481103676f9e3aaadb20
Daniel Peretz <danielperetz620@gmail.com>
2025-08-25 15:47:09 +0300

    Clean code

---
+++ src/cmps/Svgs.jsx
@@ -0,0 +1,21 @@
+export const Svgs = {
+  logo: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      viewBox="0 0 24 24"
+      className="e-91000-logo e-91000-baseline"
+      aria-label="Spotify"
+      aria-hidden="false"
+      height="32"
+      data-encore-id="logoSpotify"
+      style={{ "--encore-logo-fill-color": "var(--decorative-base)" }}
+    >
+      <title>Spotify</title>
+      <path
+        d="M13.427.01C6.805-.253 1.224 4.902.961 11.524.698 18.147 5.853 23.728 12.476 23.99c6.622.263 12.203-4.892 12.466-11.514S20.049.272 13.427.01m5.066 17.579a.717.717 0 0 1-.977.268 14.4 14.4 0 0 0-5.138-1.747 14.4 14.4 0 0 0-5.42.263.717.717 0 0 1-.338-1.392c1.95-.474 3.955-.571 5.958-.29 2.003.282 3.903.928 5.647 1.92a.717.717 0 0 1 .268.978"
+        transform="translate(-0.95,0)"
+      />
+    </svg>
+  )
+};
