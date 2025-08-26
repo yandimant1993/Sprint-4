007609ff3df45204edfb170399130b6755f7732b
yandimant1993 <yan1993dimant@gmail.com>
2025-08-26 20:30:14 +0200

    Fix when collapsed text unshown

--- src/cmps/UserStationList.jsx
+++ src/cmps/UserStationList.jsx
@@ -1,23 +1,19 @@
-import { userService } from "../services/user";
-
-
-
-export function UserStationList({ stations }) {
-
+export function UserStationList({ stations, isCollapsed }) {
     const loggedinUser = userService.getLoggedinUser()
-    // console.log('loggedInUser',loggedinUser)
     if (!loggedinUser) return
     const filteredUserStations = stations.filter(station => station.createdBy?._id === loggedinUser._id)
-    console.log('filteredUserStations', filteredUserStations)
-    return (
 
+    return (
         <ul className="user-list">
             {filteredUserStations.map(userStation => (
-                <li className="user-station">
-                    {userStation.name}
-                    <img src={userStation.stationImgUrl}></img>
-                    {"Playlist"}
-                    {userStation.songs[0].performer}
+                <li className={`user-station ${isCollapsed ? 'collapsed' : ''}`} key={userStation._id}>
+                    <img src={userStation.stationImgUrl} alt={userStation.name} className="station-picture" />
+                    {!isCollapsed && (
+                        <>
+                            <div className="station-name">{userStation.name}</div>
+                            <div className="station-playlist">{userStation.songs?.[0]?.performer}</div>
+                        </>
+                    )}
                 </li>
             ))}
         </ul>
