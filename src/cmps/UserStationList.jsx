e52d9676298e5cfd154b7bb545ffe48d618eb262
yandimant1993 <yan1993dimant@gmail.com>
2025-08-26 20:05:23 +0200

    User lists appear in the sideNav

--- src/cmps/UserStationList.jsx
+++ src/cmps/UserStationList.jsx
@@ -5,16 +5,17 @@ import { userService } from "../services/user";
 export function UserStationList({ stations }) {
 
     const loggedinUser = userService.getLoggedinUser()
-    console.log('loggedInUser',loggedinUser)
-    if(!loggedinUser) return
+    // console.log('loggedInUser',loggedinUser)
+    if (!loggedinUser) return
     const filteredUserStations = stations.filter(station => station.createdBy?._id === loggedinUser._id)
-
+    console.log('filteredUserStations', filteredUserStations)
     return (
 
         <ul className="user-list">
             {filteredUserStations.map(userStation => (
                 <li className="user-station">
                     {userStation.name}
+                    <img src={userStation.stationImgUrl}></img>
                     {"Playlist"}
                     {userStation.songs[0].performer}
                 </li>
