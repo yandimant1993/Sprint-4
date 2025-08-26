220a30dae17077dd25121b03677706e7995f2b84
avishaigal <avishai.gal@gmail.com>
2025-08-26 23:33:51 +0300

    Quickfix stationlist move key to better spot

--- src/cmps/StationList.jsx
+++ src/cmps/StationList.jsx
@@ -15,17 +15,17 @@ export function StationList({ stations }) {
     return (
         <ul className="station-list">
             {stations.map(station =>
-                <NavLink to={`/station/${station._id}`}>
-                    <li key={station._id}>
+                <NavLink to={`/station/${station._id}`} key={station._id}>
+                    <li>
                         <StationPreview station={station} />
                         {shouldShowActionBtns(station) && <div className="actions">
                             {/* <button onClick={() => onUpdateStation(station)}>Edit</button>
                         <button onClick={() => onRemoveStation(station._id)}>x</button> */}
 
-                            </div>}
-                        </li>
-                    </NavLink>
-                )}
-            </ul>
+                        </div>}
+                    </li>
+                </NavLink>
+            )}
+        </ul>
     )
 }
