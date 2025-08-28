a5a26881dd1ea2644cf08877555739a48d3afb4f
avishaigal <avishai.gal@gmail.com>
2025-08-28 08:54:22 +0300

    Change userstationlist style

--- src/cmps/UserStationList.jsx
+++ src/cmps/UserStationList.jsx
@@ -1,4 +1,11 @@
+import { useState } from 'react'
+import { useSelector } from "react-redux"
+import { Svgs } from "./Svgs"
+
 export function UserStationList({ stations, isCollapsed }) {
+    const [isHovered, setIsHovered] = useState(false)
+    const [isPinned, setIsPinned] = useState(false)
+
     const loggedinUser = userService.getLoggedinUser()
     if (!loggedinUser) return
     const filteredUserStations = stations.filter(station => station.createdBy?._id === loggedinUser._id)
@@ -7,13 +14,19 @@ export function UserStationList({ stations, isCollapsed }) {
         <ul className="user-list">
             {filteredUserStations.map(userStation => (
                 <li className={`user-station ${isCollapsed ? 'collapsed' : ''}`} key={userStation._id}>
-                    <img src={userStation.stationImgUrl} alt={userStation.name} className="user-station-picture" />
+                    <img src={userStation.stationImgUrl} alt={userStation.name} className="user-station-img" />
                     {!isCollapsed && (
-                       <div className="user-station-details grid">
+                        <div className="user-station-details flex">
 
                             <div className="user-station-name">{userStation.name}</div>
-                            <div className="user-station-playlist">{userStation.songs?.[0]?.performer}</div>
-                       </div>
+                            <div className="user-station-playlist flex">
+                                <div>
+                                    {/* {Svgs?.isPinned} */}
+                                </div>
+                                <div>
+                                    <p className="user-station-playlist-info">Playlist&nbsp;•&nbsp;{loggedinUser.fullname}</p></div>
+                            </div>
+                        </div>
                     )}
                 </li>
             ))}
