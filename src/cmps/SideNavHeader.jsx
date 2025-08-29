b35f84caea973fde2402f7d21d59b1d218e77c74
yandimant1993 <yan1993dimant@gmail.com>
2025-08-29 13:55:14 +0200

    sideNav css

--- src/cmps/SideNavHeader.jsx
+++ src/cmps/SideNavHeader.jsx
@@ -1,4 +1,5 @@
 import { Svgs } from './Svgs.jsx'
+import React from 'react'
 
 export function SideNavHeader({ setIsExpanded, isExpanded, createStation }) {
 	function toggleExpand() {
@@ -6,20 +7,30 @@ export function SideNavHeader({ setIsExpanded, isExpanded, createStation }) {
 	}
 
 	return (
-		<section className="sidenav-header flex">
-			<button className="btn-sidenav-toggle" onClick={toggleExpand}>
-				{isExpanded ? 'Your Library' : Svgs.expandSidenavIcon}
-			</button>
-			<button className="btn-create-station flex" onClick={createStation}>
-				{Svgs.addIcon}
-				{isExpanded && ' Create'}
-			</button>
-			<input
-				type="text"
-				placeholder="Search in Playlist"
-				// value={filter}
-				onChange={(e) => setFilter(e.target.value)}
-			/>
-		</section>
+		<React.Fragment>
+			<section className="sidenav-header flex">
+				<button className="btn-sidenav-toggle" onClick={toggleExpand}>
+					{isExpanded ? 'Your Library' : Svgs.expandSidenavIcon}
+				</button>
+				<button className="btn-create-station flex" onClick={createStation}>
+					{Svgs.addIcon}
+					{isExpanded && ' Create'}
+				</button>
+			</section>
+			<div className="button-playlist">
+				<button className="playlist">PlayLists</button>
+			</div>
+			<div className="header-input">
+				<div className="input-svg-container">
+					{Svgs.searchIcon}
+					<input
+						type="text"
+						placeholder="Enter Playlist"
+						// value={filter}
+						onChange={(e) => setFilter(e.target.value)}
+					/>
+				</div>
+			</div>
+		</React.Fragment>
 	)
 }
--- src/cmps/Svgs.jsx
+++ src/cmps/Svgs.jsx
@@ -162,39 +162,50 @@ export const Svgs = {
   ),
   threeDotsIcon: (
     <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    role="img"
-    aria-hidden="true"
-    fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      role="img"
+      aria-hidden="true"
+      fill="currentColor"
     >
-    <path d="M4.5 13.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3m15 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3m-7.5 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3" />
-  </svg>
+      <path d="M4.5 13.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3m15 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3m-7.5 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3" />
+    </svg>
   ),
   expandSidenavIcon: (
     <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    role="img"
-    aria-hidden="true"
-    fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      role="img"
+      aria-hidden="true"
+      fill="currentColor"
     >
-    <path d="M14.457 15.207a1 1 0 0 1-1.414-1.414L14.836 12l-1.793-1.793a1 1 0 0 1 1.414-1.414l2.5 2.5a1 1 0 0 1 0 1.414z" />
-    <path d="M20 22a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2zM4 20V4h4v16zm16 0H10V4h10z" />
-  </svg>
+      <path d="M14.457 15.207a1 1 0 0 1-1.414-1.414L14.836 12l-1.793-1.793a1 1 0 0 1 1.414-1.414l2.5 2.5a1 1 0 0 1 0 1.414z" />
+      <path d="M20 22a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2zM4 20V4h4v16zm16 0H10V4h10z" />
+    </svg>
   ),
   collapseSidenavIcon: (
     <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 16 16"
-    role="img"
-    aria-hidden="true"
-    fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 16"
+      role="img"
+      aria-hidden="true"
+      fill="currentColor"
     >
-    <path d="M10.97 5.47a.75.75 0 1 1 1.06 1.06L10.56 8l1.47 1.47a.75.75 0 1 1-1.06 1.06l-2-2a.75.75 0 0 1 0-1.06z" />
-    <path d="M1 0a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1zm.5 1.5H5v13H1.5zm13 13h-8v-13h8z" />
-  </svg>
-)
+      <path d="M10.97 5.47a.75.75 0 1 1 1.06 1.06L10.56 8l1.47 1.47a.75.75 0 1 1-1.06 1.06l-2-2a.75.75 0 0 1 0-1.06z" />
+      <path d="M1 0a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1zm.5 1.5H5v13H1.5zm13 13h-8v-13h8z" />
+    </svg>
+  ),
+  searchIcon: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      aria-hidden="true"
+      viewBox="0 0 16 16"
+      fill="currentColor"
+    >
+      <path d="M7 1.75a5.25 5.25 0 1 0 0 10.5 5.25 5.25 0 0 0 0-10.5M.25 7a6.75 6.75 0 1 1 12.096 4.12l3.184 3.185a.75.75 0 1 1-1.06 1.06L11.304 12.2A6.75 6.75 0 0 1 .25 7"></path>
+    </svg>
+  )
 }
 
 
