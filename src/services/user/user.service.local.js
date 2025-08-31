0b2cba7d529c46b0da987b54fd9f7ab8174ffff8
yandimant1993 <yan1993dimant@gmail.com>
2025-08-31 22:09:26 +0200

    Tal small changes

--- src/services/user/user.service.local.js
+++ src/services/user/user.service.local.js
@@ -57,9 +57,11 @@ async function login(userCred) {
 
 async function signup(userCred) {
     if (!userCred.imgUrl) userCred.imgUrl = 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png'
-    
-
-    const user = await storageService.post('user', userCred)
+    let user = await storageService.post('user', userCred)
+    saveLoggedinUser(user)
+    const {_id: stationId} =  await stationService.save({...stationService.getEmptyStation(), type: 'liked'})
+    user.likedStationId = stationId
+    user = await storageService.put('user', userCred)
     return saveLoggedinUser(user)
 }
 
