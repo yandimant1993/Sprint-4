3e318b40b034221a37edd61655645873cf182d0b
Daniel Peretz <danielperetz620@gmail.com>
2025-08-31 10:53:54 +0300

    Handling insertion logic

--- src/cmps/SearchStationResult.jsx
+++ src/cmps/SearchStationResult.jsx
@@ -1,50 +1,54 @@
-export function SearchStationResult({ videos = [], onVideoClick, onAddSong }) {
-    if (!Array.isArray(videos)) videos = []
+import { useDispatch } from "react-redux";
+import { addSong } from "../store/actions/player.actions";
 
-    return (
-        <section className="songs-list grid">
-            {videos.map((video, index) => (
-                <div
-                    key={video.id || index}
-                    className="songs-list-row grid video-item"
-                    onClick={() => onVideoClick(video)}
-                >
-                    <div className="song-list-number-res">{index + 1}</div>
+export function SearchStationResult({ videos = [], onVideoClick }) {
+  const dispatch = useDispatch();
+  const safeVideos = Array.isArray(videos) ? videos : [];
 
-                    <div className="song-list-title-res flex">
-                        <img
-                            src={video.thumbnail}
-                            alt={video.title}
-                            className="song-img"
-                            width="40"
-                            height="40"
-                        />
-                        <div className="song-info-text-res grid">
-                            <span className="song-title">{video.title}</span>
-                            {video.artist && <span className="song-artist">{video.artist}</span>}
-                        </div>
-                    </div>
+  return (
+    <section className="songs-list grid">
+      {safeVideos.map((video, index) => (
+        <div
+          key={video.id || index}
+          className="songs-list-row grid video-item"
+          onClick={() => onVideoClick(video)}
+        >
+          <div className="song-list-number-res">{index + 1}</div>
 
-                    <div className="song-list-album-res">{video.album || "--"}</div>
+          <div className="song-list-title-res flex">
+            <img
+              src={video.thumbnail}
+              alt={video.title}
+              className="song-img"
+              width="40"
+              height="40"
+            />
+            <div className="song-info-text-res grid">
+              <span className="song-title">{video.title}</span>
+              {video.artist && <span className="song-artist">{video.artist}</span>}
+            </div>
+          </div>
 
-                    <div className="song-list-duration-res">{video.duration || "--:--"}</div>
+          <div className="song-list-album-res">{video.album || "--"}</div>
+          <div className="song-list-duration-res">{video.duration || "--:--"}</div>
 
-                    <div className="hover-btns-actions-res flex">
-                        <button
-                            className="btn-song-add"
-                            onClick={(e) => {
-                                e.stopPropagation();
-                                onAddSong(video);
-                            }}
-                        >
-                            Add
-                        </button>
-                    </div>
-                </div>
-            ))}
-        </section>
-    )
+          <div className="hover-btns-actions-res flex">
+            <button
+  className="btn-song-add"
+  onClick={(e) => {
+    e.stopPropagation();
+    dispatch(addSong(video)); 
+  }}
+>
+  Add
+</button>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
 }
+
 
 
 
--- src/store/actions/player.actions.js
+++ src/store/actions/player.actions.js
@@ -1,38 +1,67 @@
 import { store } from '../store'
 
-
 export const SET_CURRENT_STATION = 'SET_CURRENT_STATION'
 export const SET_IS_PLAYING = 'SET_IS_PLAYING'
+export const ADD_SONG = 'ADD_SONG'
+export const REMOVE_SONG = 'REMOVE_SONG'
 
-export function setPlayer(player) {
-    store.dispatch({ type: 'SET_PLAYER', player })
+export function setPlayer(player) { store.dispatch({ type: 'SET_PLAYER', player }) }
+export function setPlaying(isPlaying) { store.dispatch({ type: 'SET_PLAYING', isPlaying }) }
+export function setCurrentTime(time) { store.dispatch({ type: 'SET_CURRENT_TIME', time }) }
+export function setDuration(duration) { store.dispatch({ type: 'SET_DURATION', duration }) }
+export function setVolume(volume) { store.dispatch({ type: 'SET_VOLUME', volume }) }
+export function toggleMute() { store.dispatch({ type: 'TOGGLE_MUTE' }) }
+export function setCurrentStation(station) { store.dispatch({ type: SET_CURRENT_STATION, station }) }
+export function setIsPlaying(isPlaying) { store.dispatch({ type: SET_IS_PLAYING, isPlaying }) }
+export function addSong(song) {
+    return { type: ADD_SONG, song }
 }
-
-export function setPlaying(isPlaying) {
-    store.dispatch({ type: 'SET_PLAYING', isPlaying })
-}
-
-export function setCurrentTime(time) {
-    store.dispatch({ type: 'SET_CURRENT_TIME', time })
-}
-
-export function setDuration(duration) {
-    store.dispatch({ type: 'SET_DURATION', duration })
-}
-
-export function setVolume(volume) {
-    store.dispatch({ type: 'SET_VOLUME', volume })
-}
-
-export function toggleMute() {
-    store.dispatch({ type: 'TOGGLE_MUTE' })
+export function removeSong(songId) {
+    return { type: REMOVE_SONG, songId }
 }
 
 
-export function setCurrentStation(station) {
-    store.dispatch({ type: SET_CURRENT_STATION, station })
-}
+// import { store } from '../store'
 
-export function setIsPlaying(isPlaying) {
-    store.dispatch({ type: SET_IS_PLAYING, isPlaying })
-}
+
+// export const SET_CURRENT_STATION = 'SET_CURRENT_STATION'
+// export const SET_IS_PLAYING = 'SET_IS_PLAYING'
+
+// export function setPlayer(player) {
+//     store.dispatch({ type: 'SET_PLAYER', player })
+// }
+
+// export function setPlaying(isPlaying) {
+//     store.dispatch({ type: 'SET_PLAYING', isPlaying })
+// }
+
+// export function setCurrentTime(time) {
+//     store.dispatch({ type: 'SET_CURRENT_TIME', time })
+// }
+
+// export function setDuration(duration) {
+//     store.dispatch({ type: 'SET_DURATION', duration })
+// }
+
+// export function setVolume(volume) {
+//     store.dispatch({ type: 'SET_VOLUME', volume })
+// }
+
+// export function toggleMute() {
+//     store.dispatch({ type: 'TOGGLE_MUTE' })
+// }
+
+
+// export function setCurrentStation(station) {
+//     store.dispatch({ type: SET_CURRENT_STATION, station })
+// }
+
+// export function setIsPlaying(isPlaying) {
+//     store.dispatch({ type: SET_IS_PLAYING, isPlaying })
+// }
+// export function addSong(song) {
+//     store.dispatch({ type: ADD_SONG, song })
+// }
+// export function removeSong(songId) {
+//     store.dispatch({ type: REMOVE_SONG, songId })
+// }
