2742a5de2449e4a53dd30b5fd856ad60f70d42d1
avishaigal <avishai.gal@gmail.com>
2025-09-01 11:12:07 +0300

    Temp quickfix in login to be reviewed

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -5,24 +5,24 @@ import { useParams } from 'react-router-dom'
 import { loadUser } from '../store/actions/user.actions'
 import { store } from '../store/store'
 import { showSuccessMsg } from '../services/event-bus.service'
-// import { socketService, SOCKET_EVENT_USER_UPDATED, SOCKET_EMIT_USER_WATCH } from '../services/socket.service'
+import { socketService, SOCKET_EVENT_USER_UPDATED, SOCKET_EMIT_USER_WATCH } from '../services/socket.service'
 
 export function UserDetails() {
 
   const params = useParams()
   const user = useSelector(storeState => storeState.userModule.watchedUser)
 
-  useEffect(() => {
-    loadUser(params.id)
+  // useEffect(() => {
+  //   loadUser(params.id)
 
-    socketService.emit(SOCKET_EMIT_USER_WATCH, params.id)
-    socketService.on(SOCKET_EVENT_USER_UPDATED, onUserUpdate)
+  //   socketService.emit(SOCKET_EMIT_USER_WATCH, params.id)
+  //   socketService.on(SOCKET_EVENT_USER_UPDATED, onUserUpdate)
 
-    return () => {
-      socketService.off(SOCKET_EVENT_USER_UPDATED, onUserUpdate)
-    }
+  //   return () => {
+  //     socketService.off(SOCKET_EVENT_USER_UPDATED, onUserUpdate)
+  //   }
 
-  }, [params.id])
+  // }, [params.id])
 
   function onUserUpdate(user) {
     showSuccessMsg(`This user ${user.fullname} just got updated from socket, new score: ${user.score}`)
